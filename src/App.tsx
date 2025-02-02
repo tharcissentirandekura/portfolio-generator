cab912f7f0b548cea329087f31753dc0ab00dae6
Tharcisse Ntirandekura <tntirand@oberlin.edu>
2025-02-02 15:23:01 -0500

    save changes by adding workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
