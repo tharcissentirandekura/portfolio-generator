69d57c9b11d88cedef379a1c5c5ad310b3b5cbb6
tharcissentirandekura <tharcissentirandekura@users.noreply.github.com>
2026-09-04 00:30:47 +0000

    deploy: 520e17d42207f869334b498634948e481bc44765

---
+++ assets/swimlanesDiagram-VR7AAH4N-DfFJvVlh.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-rgGXJDLT.js";import{_ as a}from"./mermaid.core-DK190aSV.js";import"./chunk-5VM5RSS4-DfrdsBTA.js";import"./chunk-XXDRQBXY-CbW1Gufp.js";import"./chunk-POPQ4Y6H-DfQlZZqa.js";import"./chunk-F27PBJKO-B59m3Ikb.js";import"./channel-BVa5emGh.js";import"./index-CWhHjEB7.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
